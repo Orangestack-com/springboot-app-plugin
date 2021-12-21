05ab18ccff16b2552f4aa66fb457c8f4f27a660d
Luis Eduardo Marques <luis.marques@zup.com.br>
2021-12-21 15:43:00 -0300

    chore: remove duplicate  dependencies

--- templates/kotlin/build.gradle.kts
+++ templates/kotlin/build.gradle.kts
@@ -20,13 +20,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     {% if inputs.starter-web %}
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     {% endif %}
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 {% if inputs.spring-native %}
