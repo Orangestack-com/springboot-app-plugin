bcbcabc5005f8a8668578cccecc47d3747822ba8
Luis Eduardo Marques <luis.marques@zup.com.br>
2021-12-21 14:16:42 -0300

    build: add Spring Boot Starter dependencies and add new input that allow to user choose whether want to use Spring Native

--- templates/kotlin/build.gradle.kts
+++ templates/kotlin/build.gradle.kts
@@ -1,28 +1,39 @@
+{% if inputs.spring-native == true %}
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+{% endif %}
 
 plugins {
-    kotlin("plugin.spring") version "1.6.0"
     id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    {% if inputs.spring-native == true %}
     id("org.springframework.experimental.aot") version "0.11.0"
+    {% endif %}
+    kotlin("plugin.spring") version "1.6.0"
 }
 
 repositories {
     mavenCentral()
+    {% if inputs.spring-native == true %}
     maven { url = uri("https://repo.spring.io/release") }
+    {% endif %}
 }
 
 dependencies {
-    {% if inputs.starter-web == "YES" %}
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    {% if inputs.starter-web == true %}
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     {% endif %}
 }
 
+{% if inputs.spring-native == true %}
 tasks.withType<BootBuildImage> {
     builder = "paketobuildpacks/builder:tiny"
     environment = mapOf(
         "BP_NATIVE_IMAGE" to "true"
     )
 }
+{% endif %}
--- templates/kotlin/settings.gradle.kts
+++ templates/kotlin/settings.gradle.kts
@@ -1,6 +1,8 @@
+{% if inputs.spring-native == true %}
 pluginManagement {
     repositories {
         maven { url = uri("https://repo.spring.io/release") }
         gradlePluginPortal()
     }
 }
+{% endif %}
