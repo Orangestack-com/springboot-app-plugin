51d9f55357eeef9552274b30d5dc87956cfc9ffc
luismarqueszup <59897633+luismarqueszup@users.noreply.github.com>
2021-12-21 14:50:14 -0300

    chore: code refactored
    Co-authored-by: José Augusto Martins Regis <jose.regis@zup.com.br>

--- templates/kotlin/build.gradle.kts
+++ templates/kotlin/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    {% if inputs.starter-web == true %}
+    {% if inputs.starter-web %}
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
