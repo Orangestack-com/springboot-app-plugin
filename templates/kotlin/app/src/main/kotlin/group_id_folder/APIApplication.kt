5722c0a7f77027747a0419cd81786b32126b5175
Rodrigo Fernando Silva <rodrigo.fernando@zup.com.br>
2021-12-23 15:58:58 -0300

    identation

--- templates/kotlin/app/src/main/kotlin/group_id_folder/APIApplication.kt
+++ templates/kotlin/app/src/main/kotlin/group_id_folder/APIApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class APIApplication
 
 fun main(args: Array<String>) {
-  runApplication<APIApplication>(*args)
+    runApplication<APIApplication>(*args)
 }
